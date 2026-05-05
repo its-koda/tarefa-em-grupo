d6ae947718fe1d509b19150cba289791584ba92e
its-koda <marazostefany0@hotmail.com>
2026-05-05 14:58:15 -0300

    fix: resolve o conflito titulo

--- tarefas.js
+++ tarefas.js
@@ -1,10 +1,6 @@
 // tarefas.js - Sistema de Lista de Tarefas
 let tarefas = [];
-<<<<<<< HEAD
 let titulo = "SISTEMA DE TAREFAS";// <- linha do conflito
-=======
-let titulo = "LISTA DE TAREFAS V2"; // <- linha do conflito
->>>>>>> f1624610ae5e586c7c3a981a42bdca3d447d1b7d
 function adicionarTarefa(descricao) {
   tarefas.push({ descricao: descricao, concluida: false });
   console.log("Tarefa adicionada: " + descricao);
@@ -35,17 +31,6 @@ console.log("Removida: " + rem[0].descricao);
 adicionarTarefa("Tarefa B");
 removerTarefa(1); listarTarefas();
 
-<<<<<<< HEAD
-// Membro 4 - atualiza o titulo e adiciona resumo()
-function resumo() {
-let total = tarefas.length;
-let feitas = tarefas.filter(t => t.concluida).length;
-console.log("=== " + titulo + " ===");
-console.log("Total: "+total+" | Feitas: "+feitas);
-}
-// Teste final
-resumo();
-=======
 // Membro 3 - atualiza o titulo e adiciona concluirTarefa()
 function concluirTarefa(indice) {
 if (indice < 1 || indice > tarefas.length) {
@@ -57,4 +42,13 @@ console.log("Concluida: " + tarefas[indice-1].descricao);
 // Teste
 adicionarTarefa("Tarefa C");
 concluirTarefa(1); listarTarefas();
->>>>>>> f1624610ae5e586c7c3a981a42bdca3d447d1b7d
+
+// Membro 4 - atualiza o titulo e adiciona resumo()
+function resumo() {
+let total = tarefas.length;
+let feitas = tarefas.filter(t => t.concluida).length;
+console.log("=== " + titulo + " ===");
+console.log("Total: "+total+" | Feitas: "+feitas);
+}
+// Teste final
+resumo();
